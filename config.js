e65ccf032fca768a62477e6aa22bbdedeac3ac08
acrens <chengr@dxyer.com>
2017-05-11 23:42:34 +0800

    update console

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 var config = {
     token: 'acrenschengren19920902',
     appid: 'wx260e5c8c7954bec5',
-    encodingAESKey: 'Dtmqz9baoIN6EeZgiGF7PnEApS7OCbOykVXk2zgio6l'
+    encodingAESKey: 'Dtmqz9baoIN6EeZgiGF7PnEApS7OCbOykVXk2zgio6l',
+    checkSignature: true
 };
 
 module.exports = config;
