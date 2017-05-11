6599fb80432aa150dcf9e5a4960a95075c7824f6
acrens <chengr@dxyer.com>
2017-05-11 17:14:04 +0800

    add config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    token: 'acrens_QWccJ3hbBm2g6Gc24t4c',
+    token: 'acrenschengren19920902',
     appid: 'wx260e5c8c7954bec5',
     encodingAESKey: 'Dtmqz9baoIN6EeZgiGF7PnEApS7OCbOykVXk2zgio6l'
 };
