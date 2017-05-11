7d4edb910c586f3f314664a0f6a2648fffaa246f
acrens <chengr@dxyer.com>
2017-05-11 16:48:13 +0800

    更新token

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    token: 'QWccJ3hbBm2g6Gc24t4c',
+    token: 'acrens_QWccJ3hbBm2g6Gc24t4c',
     appid: 'wx260e5c8c7954bec5',
     encodingAESKey: 'Dtmqz9baoIN6EeZgiGF7PnEApS7OCbOykVXk2zgio6l'
 };
