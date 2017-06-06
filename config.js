e4820a91b932d6bf1bf4679fabf0f6fc0691657b
acrens <chengr@dxyer.com>
2017-06-06 15:03:53 +0800

    更新配置信息

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var config = {
-    token: 'acrenschengren19920902',
-    // appid: 'wx260e5c8c7954bec5',	// 订阅号
-    appid: 'wx684aa8a342ac4022',	// 测试号
-    encodingAESKey: 'Dtmqz9baoIN6EeZgiGF7PnEApS7OCbOykVXk2zgio6l',
+    token: '',
+    // appid: '',	// 订阅号
+    appid: '',	// 测试号
+    encodingAESKey: '',
     checkSignature: true,
-    appSecret: 'b3e9b6bb778f41bd90db0456b396f9e4'
+    appSecret: ''
 };
 
 module.exports = config;
